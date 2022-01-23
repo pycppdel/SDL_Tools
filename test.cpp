ebd906e1b1003e38166cb2faf6a8e1bc6f1ed95e
root <pycppdel@gmail.com>
2022-01-23 18:52:12 +0100

    autosave

--- test.cpp
+++ test.cpp
@@ -70,68 +70,117 @@ void add_block(int x, int y, int w, int h){
 
 }
 
+class TestFrame : public Frame{
+
+public:
+
+  TestFrame(){
+
+  }
+  TestFrame(SDL_Event* e, SDL_Renderer* r, SDL_Color& c) : Frame(e, r, c){
+
+    main_camera.w = 800;
+    main_camera.h = 800;
+
+    faller.add_hitbox(0, 0, 10, 10);
+    faller.left_moveable = true;
+    faller.right_moveable = true;
+    main_camera.x = 0;
+    main_camera.y = 0;
+    main_camera.w = 800;
+    main_camera.h = 800;
+    test.has_bounds = false;
+    test.can_slide = true;
+    test.can_bounce = false;
+    test.can_wall_bounce = false;
+    test.slow_down = .0F;
+    test.has_max_speed = true;
+    test.max_speed = 40;
+    //test.can_wall_bounce = true;
+    test.can_slide = false;
+    test.gravity = .2F;
+    //test.add_hitbox(0, 0, 100, 100);
+    test.add_hitbox(0, 0, 100, 100);
+    test.y = -1000;
+
+    Block.can_fall = false;
+    Block.add_hitbox(0, 0, 100, 100);
+    Block2.can_fall = false;
+    Block2.add_hitbox(0, 0, 200, 200);
+    Block4.add_hitbox(0, 0, 300, 100);
+    Block4.can_fall = false;
+    Block3.add_hitbox(0, 0, 30, 50);
+    Block3.can_fall = false;
+    quit = false;
+
+    auto_engine.set_color(white);
+    auto_engine.set_renderer(renderer);
+
+    auto_engine.register_object((PhysicObject*)&test);
+    auto_engine.register_object((PhysicObject*)&Block);
+    auto_engine.register_object((PhysicObject*)&Block2);
+    auto_engine.register_object((PhysicObject*)&Block4);
+    auto_engine.register_object((PhysicObject*)&Block3);
+    auto_engine.register_object((PhysicObject*)&faller);
+
+    auto_engine.load_object((PhysicObject*)&test);
+    auto_engine.load_object((PhysicObject*)&Block);
+    auto_engine.load_object((PhysicObject*)&Block2);
+    auto_engine.load_object((PhysicObject*)&Block4);
+    auto_engine.load_object((PhysicObject*)&Block3);
+
+    for(int i = 0; i < 1000; i++){
+
+      add_block(rand()%800, -(rand()%100000), rand()%100+50, rand()%100+20);
+
+    }
+
+  }
+
+  void execute_all(){
+
+    main_camera.x = test.x-400;
+    main_camera.y = test.y-400;
+
+    uint8_t* state = const_cast<uint8_t*>(SDL_GetKeyboardState(NULL));
+
+    if (state[SDL_SCANCODE_LEFT]){
+
+      test.move_left(10);
+
+    }
+    if (state[SDL_SCANCODE_RIGHT]){
+
+      test.move_right(10);
+
+    }
+    if(state[SDL_SCANCODE_SPACE] && jump_count < 2){
+
+      test.jump(15);
+      jump_count++;
+
+    }
+
+    if (state[SDL_SCANCODE_A]){
+
+
+
+
+    }
+
+  }
+
+};
+
+TestFrame fram;
+
 int main(int argc, char** argv){
   init();
 
   srand(time(NULL));
 
-
-  main_camera.w = 800;
-  main_camera.h = 800;
-
-  faller.add_hitbox(0, 0, 10, 10);
-  faller.left_moveable = true;
-  faller.right_moveable = true;
-  main_camera.x = 0;
-  main_camera.y = 0;
-  main_camera.w = 800;
-  main_camera.h = 800;
-  test.has_bounds = false;
-  test.can_slide = true;
-  test.can_bounce = false;
-  test.can_wall_bounce = false;
-  test.slow_down = .0F;
-  test.has_max_speed = true;
-  test.max_speed = 40;
-  //test.can_wall_bounce = true;
-  test.can_slide = false;
-  test.gravity = .2F;
-  //test.add_hitbox(0, 0, 100, 100);
-  test.add_hitbox(0, 0, 100, 100);
-  test.y = -1000;
-
-  Block.can_fall = false;
-  Block.add_hitbox(0, 0, 100, 100);
-  Block2.can_fall = false;
-  Block2.add_hitbox(0, 0, 200, 200);
-  Block4.add_hitbox(0, 0, 300, 100);
-  Block4.can_fall = false;
-  Block3.add_hitbox(0, 0, 30, 50);
-  Block3.can_fall = false;
-  quit = false;
-
-  auto_engine.set_color(white);
-  auto_engine.set_renderer(renderer);
-
-  auto_engine.register_object((PhysicObject*)&test);
-  auto_engine.register_object((PhysicObject*)&Block);
-  auto_engine.register_object((PhysicObject*)&Block2);
-  auto_engine.register_object((PhysicObject*)&Block4);
-  auto_engine.register_object((PhysicObject*)&Block3);
-  auto_engine.register_object((PhysicObject*)&faller);
-
-  auto_engine.load_object((PhysicObject*)&test);
-  auto_engine.load_object((PhysicObject*)&Block);
-  auto_engine.load_object((PhysicObject*)&Block2);
-  auto_engine.load_object((PhysicObject*)&Block4);
-  auto_engine.load_object((PhysicObject*)&Block3);
-
-  for(int i = 0; i < 1000; i++){
-
-    add_block(rand()%800, -(rand()%100000), rand()%100+50, rand()%100+20);
-
-  }
-
+  TestFrame t(&e, renderer, white);
+  fram = t;
 
   while (!quit){
   framer.tick();
@@ -181,37 +230,10 @@ void draw(){
 void frame(void* n){
 
     SDL_PollEvent(&e);
-    uint8_t* state = const_cast<uint8_t*>(SDL_GetKeyboardState(NULL));
-
-    if (state[SDL_SCANCODE_LEFT]){
-
-      test.move_left(10);
-
-    }
-    if (state[SDL_SCANCODE_RIGHT]){
-
-      test.move_right(10);
-
-    }
-    if(state[SDL_SCANCODE_SPACE] && jump_count < 2){
-
-      test.jump(15);
-      jump_count++;
-
-    }
-
-    if (state[SDL_SCANCODE_A]){
 
 
 
-
-    }
-
-
-
-    main_camera.x = test.x-400;
-    main_camera.y = test.y-400;
-
+    fram.execute_all();
 
 
     switch(e.type){
