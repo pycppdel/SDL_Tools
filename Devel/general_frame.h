4207a37f4335c85337ae3101e37f74cb1598a29b
root <pycppdel@gmail.com>
2022-01-23 18:36:12 +0100

    autosave

--- Devel/general_frame.h
+++ Devel/general_frame.h
@@ -6,6 +6,14 @@
 #ifndef __SDL_TOOLS_DEVEL_GENERAL_PURPOSE_FRAME__
 #define __SDL_TOOLS_DEVEL_GENERAL_PURPOSE_FRAME__
 
+#ifndef __ALL_PHYSICS_IMPORT__
+#include "../Physic/physic.h"
+#endif
+
+#ifndef __SDL_TOOLS_AUTO_ENGINE__
+#include "auto_engine.h"
+#endif
+
 class General_Frame{
 
 protected:
@@ -21,6 +29,13 @@ public:
 
   virtual ~General_Frame();
   virtual void execute_all()=0;
+
+  //loading operations
+  void load_into(Object_Engine*);
+  void load_into(Auto_Engine*);
+
+  void unload_from(Object_Engine*);
+  void unload_from(Auto_Engine*);
 
 };
 
@@ -39,6 +54,26 @@ General_Frame::General_Frame(std::vector<PhysicObject*>& objs){
 }
 
 General_Frame::~General_Frame(){
+
+}
+
+void General_Frame::load_into(Object_Engine* engine){
+
+  for (PhysicObject* obj : objects){
+
+    engine.register_object(obj);
+
+  }
+
+}
+
+void General_Frame::load_into(Auto_Engine* engine){
+
+  for (PhysicObject* obj : objects){
+
+    engine.register_object(obj);
+
+  }
 
 }
 
