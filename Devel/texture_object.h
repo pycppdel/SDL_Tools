f5a4aee2c6521fa1efad3de393ab88426d82de6f
pycppdel <pycppdel@gmail.com>
2022-02-06 20:30:24 +0100

    auto

--- Devel/texture_object.h
+++ Devel/texture_object.h
@@ -14,18 +14,44 @@
 #include "../Physic/physic.h"
 #endif
 
-#define STANDARD_TGROUNDOBJECT_COLOR {0xDC,0xDC,0xDC}
+#define STANDARD_TGROUNDOBJECT_COLOR {0xA9,0xA9,0xA9}
 
-//class for texturalized ground object
-class TGroundObject : public GroundObject{
+class TextureObject_For_Physic{
 
 protected:
 
   SDL_Rect standard_rect;
+  SDL_Color standard_color = STANDARD_TGROUNDOBJECT_COLOR;
 
 public:
 
   SDL_Texture* texture = NULL;
+  //for drawing purpose
+  float angle = .0F;
+
+  TextureObject_For_Physic();
+
+  virtual void update_position()=0;
+  virtual void draw(void*)=0;
+
+
+
+};
+
+//class for texturalized ground object
+class TGroundObject : public GroundObject, public TextureObject_For_Physic{
+
+protected:
+
+  SDL_Rect standard_rect;
+  SDL_Color standard_color = STANDARD_TGROUNDOBJECT_COLOR;
+
+public:
+
+  SDL_Texture* texture = NULL;
+
+  //for drawing purpose
+  float angle = .0F;
 
   TGroundObject(int, int, int, int, int);
   TGroundObject(int, int, int, int, int, SDL_Texture*);
@@ -33,35 +59,69 @@ public:
   TGroundObject(int, int, int, int, int, bool);
 
   virtual void draw(void*);
+  virtual void update_position();
 
 
 
 };
 
-TGroundObject::TGroundObject(int x, int y, int w, int h, int ground) : GroundObject(x, y, w, h, ground){
+TextureObject_For_Physic::TextureObject_For_Physic(){
+
+}
+
+
+
+TGroundObject::TGroundObject(int x, int y, int w, int h, int ground) : GroundObject(x, y, w, h, ground), TextureObject_For_Physic(){
 
 
 }
 
-TGroundObject::TGroundObject(int x, int y, int w, int h, int ground, SDL_Texture* t) : GroundObject(x, y, w, h, ground){
+TGroundObject::TGroundObject(int x, int y, int w, int h, int ground, SDL_Texture* t) : GroundObject(x, y, w, h, ground), TextureObject_For_Physic(){
 
   //setting texture
   texture = t;
 
 }
 
-TGroundObject::TGroundObject(int x, int y, int w, int h, float slow, int ground) : GroundObject(x, y, w, h, slow, ground){
+TGroundObject::TGroundObject(int x, int y, int w, int h, float slow, int ground) : GroundObject(x, y, w, h, slow, ground), TextureObject_For_Physic(){
 
 
 }
 
-TGroundObject::TGroundObject(int x, int y, int w, int h, int ground, bool can_slow) : GroundObject(x, y, w, h, ground, can_slow){
+TGroundObject::TGroundObject(int x, int y, int w, int h, int ground, bool can_slow) : GroundObject(x, y, w, h, ground, can_slow), TextureObject_For_Physic(){
 
 
 }
 
-void TGroundObject::draw(void* rend){
+void TGroundObject::draw(void* data){
+
+  update_position();
+
+  //checking for texture
+  if (texture != NULL){
+
+    SDL_RenderCopyEx((SDL_Renderer*)data, texture, NULL, &standard_rect, angle, NULL, SDL_FLIP_NONE);
+
+  }
+  else{
+    SDL_SetRenderDrawColor((SDL_Renderer*)data, standard_color.r, standard_color.g, standard_color.b, 0x0);
+    //drawing standard
+    SDL_RenderFillRect((SDL_Renderer*)data, &standard_rect);
+
+  }
 
 }
+
+void TGroundObject::update_position(){
+  //setting standard_rect
+  standard_rect.x = x;
+  standard_rect.y = y;
+  standard_rect.w = width;
+  standard_rect.h = height;
+
+}
+
+
+//same for static object
 
 #endif
